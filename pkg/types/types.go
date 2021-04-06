e53ca35add43dd3ceeae9644626ecf605e44369a
Ravshan Sharifov <sharifovrav@gmail.com>
2021-04-06 14:09:15 +0500

    Настройка типов

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -6,8 +6,10 @@ type Money int64
 //Category пердставляет собой категорию, в которй был соверщен платеж(авто, аптеки, рестораны и т.д)
 type Category string
 
+//Status представляет собой статус платежа.
 type Status string
 
+//Предопределенные статусы платеже
 const (
 	StatusOk         Status = "OK"
 	StatusFail       Status = "FAIL"
